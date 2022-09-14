a9f1d89b3c422145110695da039a78ff1de5a0e9
Gabriel Andrade <gabrielfootze@gmail.com>
2022-09-14 16:52:26 -0300

    ye

--- src/components/pages/SobreMim.js
+++ src/components/pages/SobreMim.js
@@ -1,9 +1,15 @@
+import styles from './SobreMim.module.css'
+
 function SobreMim(){
     return (<>
         <h1>Sobre Mim</h1>
-        <p>Me chamo Gabriel tenho 18 anos, sou desenvolvedor de sistemas e estou focado nas tecnologias voltadas ao Front-End. Sou um apaixonado por tecnologias e movido á desafios.</p>
-        <p>Hoje já entendo tecnologias como NodeJS, Bootstrap, ReactJS etc.</p>
-        
+        <div className={styles.container}>
+        <p className={styles.sobre_mim_container}>Me chamo Gabriel tenho 18 anos,
+         sou desenvolvedor de sistemas e estou focado nas tecnologias 
+         voltadas ao Front-End. Sou um apaixonado por tecnologias e 
+         movido á desafios.</p>
+        <p className={styles.sobre_mim_container}>Hoje já entendo tecnologias como NodeJS, Bootstrap, ReactJS etc.</p>
+        </div>
         </>
     )
 
