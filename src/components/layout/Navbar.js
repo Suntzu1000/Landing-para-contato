9735cad78ab4f1ba912159514e12dedb0a4deab0
Gabriel Andrade <gabrielfootze@gmail.com>
2022-09-22 16:37:14 -0300

    ddd

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,11 +4,18 @@ import styles from './Navbar.module.css';
 import logo0 from '../../img/logo02.png'
 
 function Navbar() {
-    return(
+    return(<>
+        <div className='styles.menu_section on'>
+          <div className={styles.menu_toggle}>
+            <div className={styles.one}></div>
+            <div className={styles.two}></div>
+            <div className={styles.three}></div>
+            </div>  
+        
         <nav className={styles.navbar}>
            <Container>
             <Link to='/'>
-                <img src={logo0} alt='Dev0'/>
+                <img className={styles.img} src={logo0} alt='Dev0'/>
             </Link>
             <ul className={styles.list}>
                 <li  className={styles.item}>
@@ -34,6 +41,8 @@ function Navbar() {
             </ul>
            </Container>
         </nav>
+        </div>
+        </>
     )
 }
 
--- src/components/pages/SobreMim.js
+++ src/components/pages/SobreMim.js
@@ -1,11 +1,12 @@
-import styles from './SobreMim.module.css'
+import styles from './Home.module.css'
 
 
 function SobreMim(){
     return (<>
-        <h1 className={styles.sobremim}>Sobre Mim</h1>
-        <div className={styles.container}>
-        <p className={styles.sobre_mim_container}>Me chamo Gabriel tenho 18 anos,
+        
+        <div className={styles.home_container}>
+        <h1 >Sobre Mim</h1>
+        <p >Me chamo Gabriel tenho 18 anos,
          sou desenvolvedor de sistemas e estou focado nas tecnologias 
          voltadas ao Front-End. Sou um apaixonado por tecnologias e 
          movido á desafios. 
